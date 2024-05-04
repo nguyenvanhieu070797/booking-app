2dc486e12c25b4831f24f23106f88d58dd032db7
n-hieu <nguyenvanhieu070797@gmail.com>
2024-05-04 16:29:59 +0700

    upload code

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
+
+
